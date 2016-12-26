07459b13fcd1e3527a852ba18e804270e248a204
Dante <elvewyn@gmail.com>
2016-12-26 21:25:52 +0800

    Initial Commit

---
+++ OCESPTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCESPTouch
+//
+//  Created by Dante on 2016/12/26.
+//  Copyright © 2016年 peakhonor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

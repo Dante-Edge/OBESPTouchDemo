5dc8fd76e3704bc9495d3b5d48eadb7d08d04b7c
Dante <elvewyn@gmail.com>
2016-12-27 09:10:33 +0800

    Test

--- OCESPTouch/ViewController.m
+++ OCESPTouch/ViewController.m
@@ -7,22 +7,33 @@
 //
 
 #import "ViewController.h"
+#import "ESPTouchResult.h"
+#import "ESPTouchTask.h"
+#import <UIKit/UIKit.h>
 
 @interface ViewController ()
-
+@property (atomic, strong) ESPTouchTask *_esptouchTask;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self._esptouchTask = [[ESPTouchTask alloc] initWithApSsid:@"WiFi"
+                                                   andApBssid:@""
+                                                     andApPwd:@"7788990011"
+                                               andIsSsidHiden:NO];
+    [self._esptouchTask setEsptouchDelegate:self];
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(queue, ^{
+        ESPTouchResult *result = [self._esptouchTask executeForResult];
+        NSLog(@"%@", result.description);
+    });
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) onEsptouchResultAddedWithResult: (ESPTouchResult *) result {
+    NSLog(@"%@", result.description);
 }
 
 
